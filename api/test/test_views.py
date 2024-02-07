b427e7bb48595776e468ac527da2082cec653d14
Nicolás <wantobelieve@gmail.com>
2024-02-07 18:21:53 -0300

    test: added and improved test for `spreads` and `spreads/{market_id}` endpoint

--- api/test/test_views.py
+++ api/test/test_views.py
@@ -8,6 +8,15 @@ class TestViews(TestSetUp):
     Tests for `/spreads/` endpoint.
     """
 
+    def test_get_each_markets_spread(self):
+        """
+        Validate correct spreads list retrieving.
+        """
+        url = reverse("spread-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response["content-type"], "application/json")
+
     def test_get_market_spread(self):
         """
         Validate correct spread retrieving.
@@ -15,3 +24,4 @@ class TestViews(TestSetUp):
         url = reverse("spread-detail", kwargs={"pk": self.valid_market_id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response["content-type"], "application/json")
