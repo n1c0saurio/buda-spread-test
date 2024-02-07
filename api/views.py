fcc7f6598e834bb52784424054dc2808809848a7
Nicolás <wantobelieve@gmail.com>
2024-02-07 18:20:46 -0300

    feat: added view for listing spread for all markets

--- api/views.py
+++ api/views.py
@@ -6,6 +6,11 @@ from rest_framework.response import Response
 
 class SpreadViewSet(viewsets.ViewSet):
 
+    def list(self, request):
+        spreads = Spread.get_each_markets_spread()
+        serializer = SpreadSerializer(spreads, many=True)
+        return Response(serializer.data)
+
     def retrieve(self, request, pk=None):
         spread = Spread(pk)
         serializer = SpreadSerializer(spread)
