3fc1e89aa36bf34127ebdbe0e4f2eaff596bbb2c
Nicolás <wantobelieve@gmail.com>
2024-02-08 00:27:06 -0300

    test: added test for `/spreads/{market_id}/save` endpoint

--- api/test/test_views.py
+++ api/test/test_views.py
@@ -1,6 +1,8 @@
 from .test_setup import TestSetUp
 from django.urls import reverse
 from rest_framework import status
+from decimal import Decimal
+from ..models import Spread
 
 
 class TestViews(TestSetUp):
@@ -25,3 +27,17 @@ class TestViews(TestSetUp):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response["content-type"], "application/json")
+
+    def test_save_spread(self):
+        """
+        Validate if a spread is saved on database
+        """
+        url = reverse("spread-save", kwargs={"pk": self.valid_market_id})
+        response = self.client.get(url)
+
+        # find a spread with the same value as the response data
+        spread_value = Decimal(response.json()["value"])
+        spread_saved = Spread.objects.get(value=spread_value)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(spread_saved)
