2e688ba484459b1ea116481c86985c284f34dd3b
Nicolás <wantobelieve@gmail.com>
2024-02-08 15:29:35 -0300

    feat: added `/spreads/{market_id}/polling` endpoint

--- api/serializer.py
+++ api/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Spread
+from .models import Spread, Polling
 
 
 class SpreadSerializer(serializers.ModelSerializer):
@@ -8,3 +8,27 @@ class SpreadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Spread
         fields = "__all__"
+
+
+class PollingSerializer(serializers.ModelSerializer):
+    """Serializer for `Polling` model"""
+
+    # virtual fields to show amounts in the same fashion as Buda.com API
+    current_spread = serializers.SerializerMethodField()
+    stored_spread = serializers.SerializerMethodField()
+
+    def get_current_spread(self, polling):
+        return [polling.current_spread_value, polling.current_spread_currency]
+
+    def get_stored_spread(self, polling):
+        return [polling.stored_spread_value, polling.stored_spread_currency]
+
+    class Meta:
+        model = Polling
+        fields = [
+            "market_id",
+            "current_spread",
+            "stored_spread",
+            "result",
+            "stored_spread_date",
+        ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
-from .models import Spread
-from .serializer import SpreadSerializer
+from .models import Spread, Polling
+from .serializer import SpreadSerializer, PollingSerializer
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -23,3 +23,20 @@ class SpreadViewSet(viewsets.ViewSet):
         spread.save()
         serializer = SpreadSerializer(spread)
         return Response(serializer.data, status.HTTP_201_CREATED)
+
+    @action(detail=True)
+    def polling(self, request, pk=None):
+        """
+        Return a comparison between the current market spread
+        vs. the latest stored one for the same market.
+        """
+        try:
+            stored_spreads = Spread.objects.filter(market_id__iexact=pk)
+            polling = Polling.create(stored_spreads.latest("fetch_date"))
+            serializer = PollingSerializer(polling)
+        except Spread.DoesNotExist:
+            return Response(
+                {"message": "No stored spread was found for this market"},
+                status.HTTP_404_NOT_FOUND,
+            )
+        return Response(serializer.data)
